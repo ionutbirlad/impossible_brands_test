91c2f6118e5b00c8b02ff0ff4391d464b4ec6cd4
ionutbirlad <birladionut@yahoo.it>
2022-02-11 21:27:16 +0100

    [Products creation] mocked up data for the T shirts in the requested variants

--- products/main.js
+++ products/main.js
@@ -1,22 +1,21 @@
 import { baseAdminApi } from './APIs.js'
 import './dotenv.config.js'
-import { products } from './products.js'
+import { products, variants } from './products.js'
 
 
 async function fetchProducts() {
-  const query = `mutation {
+  const query = `mutation productCreate($input: ProductInput!, $media: [CreateMediaInput!]) {
       ${products.map(product => {
-        return `Product${products.indexOf(product)}: productCreate(input: {
-                  title: "${product.title}"
-                }) {
+        return `Product${products.indexOf(product)}: productCreate(input: $input, media: $media) {
                   product {
                     id
                   }
                 }`
       }).join(" ")}
   }`
+  const variables = variants
 
-  baseAdminApi(query)
+  baseAdminApi(query, variables)
 }
 
 fetchProducts()
--- products/products.js
+++ products/products.js
@@ -12,3 +12,56 @@ export const products = [
     "description": "Third product description"
   }
 ]
+
+export const variants = {
+  "media": [
+    {
+      "mediaContentType": "IMAGE",
+      "originalSource": "https://media.istockphoto.com/photos/red-tshirt-clothes-picture-id685779142?s=612x612"
+    },
+    {
+      "mediaContentType": "IMAGE",
+      "originalSource": "https://media.istockphoto.com/photos/blue-tshirts-front-and-back-used-as-design-template-picture-id831591110?s=612x612"
+    },
+    {
+      "mediaContentType": "IMAGE",
+      "originalSource": "https://media.istockphoto.com/photos/green-tshirt-clothes-picture-id685724606?s=612x612"
+    },
+  ],
+  "input": {
+    "title": "T-shirt",
+    "metafields": {
+      "description": ""
+    },
+    "published": true,
+    "options": [
+      "Color", "Size"
+    ],
+    "variants": [
+      {
+        "mediaSrc": ["https://media.istockphoto.com/photos/red-tshirt-clothes-picture-id685779142?s=612x612"],
+        "options": ["Red", "M"]
+      },
+      {
+        "mediaSrc": ["https://media.istockphoto.com/photos/red-tshirt-clothes-picture-id685779142?s=612x612"],
+        "options": ["Red", "L"]
+      },
+      {
+        "mediaSrc": ["https://media.istockphoto.com/photos/blue-tshirts-front-and-back-used-as-design-template-picture-id831591110?s=612x612"],
+        "options": ["Blue", "M"]
+      },
+      {
+        "mediaSrc": ["https://media.istockphoto.com/photos/blue-tshirts-front-and-back-used-as-design-template-picture-id831591110?s=612x612"],
+        "options": ["Blue", "L"]
+      },
+      {
+        "mediaSrc": ["https://media.istockphoto.com/photos/green-tshirt-clothes-picture-id685724606?s=612x612"],
+        "options": ["Green", "M"]
+      },
+      {
+        "mediaSrc": ["https://media.istockphoto.com/photos/green-tshirt-clothes-picture-id685724606?s=612x612"],
+        "options": ["Green", "L"]
+      },
+    ]
+  }
+ }
