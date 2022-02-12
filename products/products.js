a74716bd6814bbc7576ec1afe45804bbfeef5e43
ionutbirlad <birladionut@yahoo.it>
2022-02-12 18:57:40 +0100

    [Minor changes]

--- products/products.js
+++ products/products.js
@@ -1,85 +1,85 @@
 export const products = [{
-    "media": [{
-        "mediaContentType": "IMAGE",
-        "originalSource": "https://media.istockphoto.com/photos/red-tshirt-clothes-picture-id685779142?s=612x612"
-      },
-      {
-        "mediaContentType": "IMAGE",
-        "originalSource": "https://media.istockphoto.com/photos/blue-tshirts-front-and-back-used-as-design-template-picture-id831591110?s=612x612"
-      },
-      {
-        "mediaContentType": "IMAGE",
-        "originalSource": "https://media.istockphoto.com/photos/green-tshirt-clothes-picture-id685724606?s=612x612"
-      },
+  "media": [{
+      "mediaContentType": "IMAGE",
+      "originalSource": "https://media.istockphoto.com/photos/man-posing-with-blank-red-shirt-picture-id513532929?s=2048x2048"
+    },
+    {
+      "mediaContentType": "IMAGE",
+      "originalSource": "https://media.istockphoto.com/photos/man-posing-with-blank-navy-blue-shirt-picture-id518593821?s=2048x2048"
+    },
+    {
+      "mediaContentType": "IMAGE",
+      "originalSource": "https://media.istockphoto.com/photos/man-posing-with-blank-green-shirt-picture-id515838111?s=2048x2048"
+    },
+  ],
+  "input": {
+    "title": "Awesome T-shirt",
+    "descriptionHtml": "Basic cotton T-shirt available in different colors and sizes.",
+    "metafields": {
+      "description": "Basic cotton T-shirt available in different colors and sizes.",
+      "type": "T-shirt"
+    },
+    "published": true,
+    "options": [
+      "Color", "Size", "Material"
     ],
-    "input": {
-      "title": "Awesome T-shirt",
-      "descriptionHtml": "Basic cotton T-shirt available in different colors and sizes.",
-      "metafields": {
-        "description": "Basic cotton T-shirt available in different colors and sizes.",
-        "type": "T-shirt"
-      },
-      "published": true,
-      "options": [
-        "Color", "Size", "Material"
-      ],
-      "variants": [{
-          "mediaSrc": ["https://media.istockphoto.com/photos/red-tshirt-clothes-picture-id685779142?s=612x612"],
-          "options": ["Red", "M", "Cotton"]
-        },
-        {
-          "mediaSrc": ["https://media.istockphoto.com/photos/red-tshirt-clothes-picture-id685779142?s=612x612"],
-          "options": ["Red", "L", "Cotton"]
-        },
-        {
-          "mediaSrc": ["https://media.istockphoto.com/photos/blue-tshirts-front-and-back-used-as-design-template-picture-id831591110?s=612x612"],
-          "options": ["Blue", "M", "Cotton"]
-        },
-        {
-          "mediaSrc": ["https://media.istockphoto.com/photos/blue-tshirts-front-and-back-used-as-design-template-picture-id831591110?s=612x612"],
-          "options": ["Blue", "L", "Cotton"]
-        },
-        {
-          "mediaSrc": ["https://media.istockphoto.com/photos/green-tshirt-clothes-picture-id685724606?s=612x612"],
-          "options": ["Green", "M", "Cotton"]
-        },
-        {
-          "mediaSrc": ["https://media.istockphoto.com/photos/green-tshirt-clothes-picture-id685724606?s=612x612"],
-          "options": ["Green", "L", "Cotton"]
-        },
-      ]
-    }
-  },
-  {
-    "media": [{
-        "mediaContentType": "IMAGE",
-        "originalSource": "https://media.istockphoto.com/photos/purple-canvas-tote-bag-isolated-on-white-background-with-clipping-picture-id1153660150?s=612x612"
+    "variants": [{
+        "mediaSrc": ["https://media.istockphoto.com/photos/man-posing-with-blank-red-shirt-picture-id513532929?s=2048x2048"],
+        "options": ["Red", "M", "Cotton"]
       },
       {
-        "mediaContentType": "IMAGE",
-        "originalSource": "https://media.istockphoto.com/photos/woman-is-holding-bag-canvas-fabric-for-mockup-blank-template-picture-id1148884819?s=612x612"
+        "mediaSrc": ["https://media.istockphoto.com/photos/man-posing-with-blank-red-shirt-picture-id513532929?s=2048x2048"],
+        "options": ["Red", "L", "Cotton"]
       },
+      {
+        "mediaSrc": ["https://media.istockphoto.com/photos/man-posing-with-blank-navy-blue-shirt-picture-id518593821?s=2048x2048"],
+        "options": ["Blue", "M", "Cotton"]
+      },
+      {
+        "mediaSrc": ["https://media.istockphoto.com/photos/man-posing-with-blank-navy-blue-shirt-picture-id518593821?s=2048x2048"],
+        "options": ["Blue", "L", "Cotton"]
+      },
+      {
+        "mediaSrc": ["https://media.istockphoto.com/photos/man-posing-with-blank-green-shirt-picture-id515838111?s=2048x2048"],
+        "options": ["Green", "M", "Cotton"]
+      },
+      {
+        "mediaSrc": ["https://media.istockphoto.com/photos/man-posing-with-blank-green-shirt-picture-id515838111?s=2048x2048"],
+        "options": ["Green", "L", "Cotton"]
+      },
+    ]
+  }
+},
+{
+  "media": [{
+      "mediaContentType": "IMAGE",
+      "originalSource": "https://media.istockphoto.com/photos/violet-dark-elegant-leather-tote-shoulder-bag-isolated-white-picture-id1304053555?s=2048x2048"
+    },
+    {
+      "mediaContentType": "IMAGE",
+      "originalSource": "https://media.istockphoto.com/photos/white-shopping-bag-picture-id535634791?s=2048x2048"
+    },
+  ],
+  "input": {
+    "title": "Fantastic bag",
+    "descriptionHtml": "Unique bag avaible in 2 fantastic colors, white and purple!",
+    "metafields": {
+      "description": "Unique bag avaible in 2 fantastic colors, white and purple!",
+      "type": "Bag"
+    },
+    "published": true,
+    "options": [
+      "Color", "Size"
     ],
-    "input": {
-      "title": "Fantastic bag",
-      "descriptionHtml": "Unique bag avaible in 2 fantastic colors, white and purple!",
-      "metafields": {
-        "description": "Unique bag avaible in 2 fantastic colors, white and purple!",
-        "type": "Bag"
+    "variants": [{
+        "mediaSrc": ["https://media.istockphoto.com/photos/violet-dark-elegant-leather-tote-shoulder-bag-isolated-white-picture-id1304053555?s=2048x2048"],
+        "options": ["Purple", "M"]
       },
-      "published": true,
-      "options": [
-        "Color", "Size"
-      ],
-      "variants": [{
-          "mediaSrc": ["https://media.istockphoto.com/photos/purple-canvas-tote-bag-isolated-on-white-background-with-clipping-picture-id1153660150?s=612x612"],
-          "options": ["Purple", "M"]
-        },
-        {
-          "mediaSrc": ["https://media.istockphoto.com/photos/woman-is-holding-bag-canvas-fabric-for-mockup-blank-template-picture-id1148884819?s=612x612"],
-          "options": ["White", "M"]
-        },
-      ]
-    }
-  },
+      {
+        "mediaSrc": ["https://media.istockphoto.com/photos/white-shopping-bag-picture-id535634791?s=2048x2048"],
+        "options": ["White", "M"]
+      },
+    ]
+  }
+},
 ]
